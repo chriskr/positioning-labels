46cafd08792180df44b1570d02c4b2b212ab59b1
Christian Krebs <christian.krebs.pi@gmail.com>
2019-05-23 14:09:31 +0200

    Fix build

---
+++ build/precache-manifest.aa0796711c8e7cbe44dc22237d675d1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758e0d94bffe1723d433a87c8ea96593",
+    "url": "/positioning-labels/build/index.html"
+  },
+  {
+    "revision": "63b942ee8f1514cff5ee",
+    "url": "/positioning-labels/build/static/css/main.a05168e0.chunk.css"
+  },
+  {
+    "revision": "9214114558ab7b5e597b",
+    "url": "/positioning-labels/build/static/js/2.81bfe7f2.chunk.js"
+  },
+  {
+    "revision": "63b942ee8f1514cff5ee",
+    "url": "/positioning-labels/build/static/js/main.bd290f7f.chunk.js"
+  },
+  {
+    "revision": "74fdeba153a811a10758",
+    "url": "/positioning-labels/build/static/js/runtime~main.2caf41cd.js"
+  }
+]);
